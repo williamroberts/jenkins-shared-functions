bc031a468b169fc6e9505a9d3b3400a49754d10b
Will Roberts <williamroberts@live.co.uk>
2016-11-03 16:33:39 +1100

    Please work...

--- vars/getAccountAlias.groovy
+++ vars/getAccountAlias.groovy
@@ -1,3 +1,7 @@
 def call(String bucketName) {
-  return sh "aws iam list-account-aliases --query 'AccountAliases[0]' --output text";
+  def accountAlias = sh (
+    script: "aws iam list-account-aliases --query 'AccountAliases[0]' --output text;",
+    returnStdout: true
+    ).trim()
+  return accountAlias;
 }
