57f13cb7de08e16ef4170ef1a390c9c54a7ee23b
Will Roberts <williamroberts@live.co.uk>
2016-11-03 13:59:21 +1100

    Correct shell command to get account alias

--- vars/getAccountAlias.groovy
+++ vars/getAccountAlias.groovy
@@ -1,3 +1,3 @@
 def call(String bucketName) {
-  return "\$(aws iam list-account-aliases --query 'AccountAliases[0]' --output text)";
+  return sh "\$(aws iam list-account-aliases --query 'AccountAliases[0]' --output text)";
 }
