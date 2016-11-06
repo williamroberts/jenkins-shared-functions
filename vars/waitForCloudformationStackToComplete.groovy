30c206d5b96fb3061e9e01f9ea40cb85277b57a8
Will Roberts <williamroberts@live.co.uk>
2016-11-07 10:19:29 +1100

    Add region to Cloudformation CLI calls

--- vars/waitForCloudformationStackToComplete.groovy
+++ vars/waitForCloudformationStackToComplete.groovy
@@ -1,8 +1,8 @@
 def call(String stackName) {
   if (stackBeingProcessedStatus == 'CREATE_IN_PROGRESS') {
-    sh "aws cloudformation wait stack-create-complete --stack-name ${stackName}"
+    sh "aws --region ${awsRegion} cloudformation wait stack-create-complete --stack-name ${stackName}"
   } else (stackBeingProcessedStatus == 'UPDATE_IN_PROGRESS') {
-    sh "aws cloudformation wait stack-update-complete --stack-name ${stackName}"
+    sh "aws --region ${awsRegion} cloudformation wait stack-update-complete --stack-name ${stackName}"
   } else (stackBeingProcessedStatus == 'ROLLBACK_IN_PROGRESS') {
     pollRollbackStack(stackName)
   } else {
@@ -11,5 +11,5 @@ def call(String stackName) {
 }
 
 def pollRollbackStack(String stackName) {
-  sh "while true; do $(aws cloudformation list-stacks --stack-status-filter ROLLBACK_COMPLETE UPDATE_ROLLBACK_COMPLETE --query 'StackSummaries[?StackName==`\"${stackName}\"`].StackStatus' --output text); if [[ $__STACK_STATUS == *'COMPLETE' ]]; then break; fi; sleep 2; done;"
+  sh "while true; do $(aws --region ${awsRegion} cloudformation list-stacks --stack-status-filter ROLLBACK_COMPLETE UPDATE_ROLLBACK_COMPLETE --query 'StackSummaries[?StackName==`\"${stackName}\"`].StackStatus' --output text); if [[ $__STACK_STATUS == *'COMPLETE' ]]; then break; fi; sleep 2; done;"
 }
