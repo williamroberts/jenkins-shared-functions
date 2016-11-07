8dc38ac4b6b324fca122a4d04d5cac5ffc371bc5
Will Roberts <williamroberts@live.co.uk>
2016-11-07 15:53:41 +1100

    Apparently I don't know how to write if statements... oh dear

--- vars/waitForCloudformationStackToComplete.groovy
+++ vars/waitForCloudformationStackToComplete.groovy
@@ -1,11 +1,11 @@
 def call(String awsRegion, String stackName, String stackStatus) {
   if (stackStatus == 'CREATE_IN_PROGRESS') {
     sh "aws --region ${awsRegion} cloudformation wait stack-create-complete --stack-name ${stackName}"
-  } else (stackStatus == 'UPDATE_IN_PROGRESS') {
+  } else if (stackStatus == 'UPDATE_IN_PROGRESS') {
     sh "aws --region ${awsRegion} cloudformation wait stack-update-complete --stack-name ${stackName}"
-  } else (stackStatus == 'DELETE_IN_PROGRESS') {
+  } else if (stackStatus == 'DELETE_IN_PROGRESS') {
     sh "aws --region ${awsRegion} cloudformation wait stack-delete-complete --stack-name ${stackName}"
-  } else (stackStatus == 'ROLLBACK_IN_PROGRESS' || stackStatus == 'UPDATE_ROLLBACK_IN_PROGRESS' || stackStatus == 'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS') {
+  } else if (stackStatus == 'ROLLBACK_IN_PROGRESS' || stackStatus == 'UPDATE_ROLLBACK_IN_PROGRESS' || stackStatus == 'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS') {
     pollRollbackStack(stackName)
   } else {
     error('Unrecognised status of Cloudformation stack ' + stackStatus ' for stack ' + stackName)
